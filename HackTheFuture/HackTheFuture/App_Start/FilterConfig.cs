96e07b068a78850e12fb44c705ee649eb674ab78
emrealtinyay <emre.altinyay@student.pxl.be>
2015-12-03 10:22:57 +0100

    start

---
+++ HackTheFuture/HackTheFuture/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HackTheFuture
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
